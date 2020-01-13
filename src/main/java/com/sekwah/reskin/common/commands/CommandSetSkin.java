8710460d6ea45a61779d95eb3bb2695096780596
Sekwah <sekwah41@gmail.com>
2020-01-13 03:55:13 +0000

    Updated Command

--- src/main/java/com/sekwah/reskin/common/commands/CommandSetSkin.java
+++ src/main/java/com/sekwah/reskin/common/commands/CommandSetSkin.java
@@ -6,10 +6,16 @@ import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.server.command.TextComponentHelper;
+
+import javax.annotation.Nullable;
+import java.util.Collections;
+import java.util.List;
 
 public class CommandSetSkin extends CommandBase {
     @Override
@@ -32,9 +38,22 @@ public class CommandSetSkin extends CommandBase {
         if(args.length < 1) {
             sender.sendMessage(TextComponentHelper.createComponentTranslation(sender, "setskin.missingargs"));
         }
-        else if(args.length == 1){
+        else if(args.length == 1) {
             EntityPlayer player = (EntityPlayer) sender;
             CustomSkinManager.setSkin(player.getUniqueID(), args[0]);
+            sender.sendMessage(TextComponentHelper.createComponentTranslation(sender, "setskin.setplayer", player.getDisplayName(), args[0]));
         }
+        else if(args.length == 2) {
+            EntityPlayer targetPlayer = getPlayer(server, sender, args[0]);
+            CustomSkinManager.setSkin(targetPlayer.getUniqueID(), args[1]);
+            sender.sendMessage(TextComponentHelper.createComponentTranslation(sender, "setskin.setplayer", targetPlayer.getDisplayName(), args[1]));
+        }
+    }
+
+    public List<String> getTabCompletions(MinecraftServer p_getTabCompletions_1_, ICommandSender p_getTabCompletions_2_, String[] p_getTabCompletions_3_, @Nullable BlockPos p_getTabCompletions_4_) {
+        if (p_getTabCompletions_3_.length == 1) {
+            return getListOfStringsMatchingLastWord(p_getTabCompletions_3_, p_getTabCompletions_1_.getOnlinePlayerNames());
+        }
+        return null;
     }
 }
