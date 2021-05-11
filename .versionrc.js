7a4a1f619b5cc5e39cd4dd7c155f63d69f1154ba
Sekwah <sekwah41@gmail.com>
2021-05-12 00:49:33 +0100

    ci: Updated versioning tools and changelog.

---
+++ .versionrc.js
@@ -0,0 +1,19 @@
+let versionRegex = /(\nversion=)([0-9.]+)/;
+
+const tracker = {
+  filename: 'gradle.properties',
+  updater: {
+    'readVersion': (contents) => {
+      return versionRegex.exec(contents)[2];
+    },
+    'writeVersion': (contents, version) => {
+      console.log(contents, version);
+      return contents.replace(versionRegex, `$1${version}`);
+    }
+  }
+}
+
+module.exports = {
+  bumpFiles: [tracker],
+  packageFiles: [tracker]
+}
