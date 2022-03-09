ff3e9e9505aaeea5e5a34003188c1e284d51a817
Sekwah <contact@sekwah.com>
2022-03-09 01:46:31 +0000

    refactor: preparing repo to switch over to using sekclib

--- fabric/src/main/java/com/sekwah/reskin/network/NetworkConst.java
+++
@@ -1,9 +0,0 @@
-package com.sekwah.reskin.network;
-
-import com.sekwah.reskin.ReSkin;
-import net.minecraft.resources.ResourceLocation;
-
-public class NetworkConst {
-    public static final ResourceLocation CLIENT_SKIN_CHANGE_PACKET = new ResourceLocation(ReSkin.MOD_ID, "client_skin_change");
-    public static final ResourceLocation CLIENT_CLEAR_SKIN_CACHE_PACKET = new ResourceLocation(ReSkin.MOD_ID, "client_clear_skin_cache");
-}
