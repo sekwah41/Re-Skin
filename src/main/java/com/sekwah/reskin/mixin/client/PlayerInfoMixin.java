db3e5c169b29d8483058c8183f91581e095f8dad
Sekwah <contact@sekwah.com>
2024-07-07 03:22:57 +0100

    fix: default skin cache lambda mixin not executing properly (#142)

--- src/main/java/com/sekwah/reskin/mixin/client/PlayerInfoMixin.java
+++ src/main/java/com/sekwah/reskin/mixin/client/PlayerInfoMixin.java
@@ -1,14 +1,17 @@
 package com.sekwah.reskin.mixin.client;
 
+import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.minecraft.MinecraftProfileTexture;
 import com.sekwah.reskin.client.ClientSkinManager;
 import com.sekwah.reskin.core.client.ClientSkinData;
 import net.minecraft.client.multiplayer.PlayerInfo;
+import net.minecraft.client.resources.SkinManager;
 import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import javax.annotation.Nullable;
@@ -16,14 +19,19 @@ import javax.annotation.Nullable;
 @Mixin(PlayerInfo.class)
 public class PlayerInfoMixin {
 
-    @Shadow @Nullable public String skinModel;
+    @Shadow
+    @Nullable
+    public String skinModel;
 
-    @Inject(method = "lambda$registerTextures$0", at = @At(value = "TAIL"))
-    protected void registerTextures(MinecraftProfileTexture.Type type, ResourceLocation resourceLocation, MinecraftProfileTexture profileTexture, CallbackInfo ci) {
-        if(type != MinecraftProfileTexture.Type.SKIN) return;
-
-        synchronized (ClientSkinManager.originalSkinMap) {
-            ClientSkinManager.originalSkinMap.put(((PlayerInfoAccessor) (Object) this).getGameProfile().getId(), new ClientSkinData(resourceLocation, this.skinModel));
-        }
+    @Redirect(method = "registerTextures", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/resources/SkinManager;registerSkins(Lcom/mojang/authlib/GameProfile;Lnet/minecraft/client/resources/SkinManager$SkinTextureCallback;Z)V"))
+    public void registerSkins(SkinManager skinManager, GameProfile gameProfile, SkinManager.SkinTextureCallback callback, boolean requireSecure) {
+        skinManager.registerSkins(gameProfile, (typeIn, location, profileTexture) -> {
+            if (typeIn == MinecraftProfileTexture.Type.SKIN) {
+                synchronized (ClientSkinManager.originalSkinMap) {
+                    ClientSkinManager.originalSkinMap.put(gameProfile.getId(), new ClientSkinData(location, this.skinModel));
+                }
+            }
+            callback.onSkinTextureAvailable(typeIn, location, profileTexture);
+        }, requireSecure);
     }
 }
